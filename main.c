3e8307cba8639058b38b87a8e670ef72b62f89e2
Dalton Overmyer <overmyerms13dalton@gmail.com>
2024-03-05 22:59:51 -0600

    querying all devices and support events

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <linux/input.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,13 +14,32 @@
 #define MAX_DEV_INPUT_PATH   255
 #define MAX_DEV_INPUT_EVENTS 20
 
-int query_input_device(int device);
+// bits and pieces taken from https://gitlab.freedesktop.org/libevdev/libevdev
+
+#define LONG_BITS (sizeof(long) * 8)
+#define NLONGS(x) (((x) + LONG_BITS - 1) / LONG_BITS)
+
+typedef struct dev_input {
+  int fd;
+  char name[256];
+  char phys[256];
+  char uniq[256];
+  struct input_id ids;
+  int driver_version;
+  unsigned long bits[NLONGS(EV_CNT)];
+  unsigned long key_bits[NLONGS(KEY_CNT)];
+  unsigned long key_values[NLONGS(KEY_CNT)];
+} dev_input_t;
+
+static inline int bit_is_set(const unsigned long* array, int bit);
+
 void fs_event_dev_input_cb(uv_fs_event_t* handle, const char* filename, int events, int status);
 
-void dev_input_map_query();
+void dev_input_scan();
+int dev_input_query(dev_input_t* dev, int fd);
 
 int main() {
-  dev_input_map_query();
+  dev_input_scan();
 
   // uv_fs_event_t dev_input_fs_event;
   // uv_fs_event_init(uv_default_loop(), &dev_input_fs_event);
@@ -51,23 +71,7 @@ void fs_event_dev_input_cb(uv_fs_event_t* handle, const char* filename, int even
   printf("[%d] %s\n", event_num, path);
 }
 
-#define _GNU_SOURCE /* for asprintf */
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <linux/input.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#define BITS_PER_LONG        (sizeof(long) * 8)
-#define NBITS(x)             ((((x)-1) / BITS_PER_LONG) + 1)
-#define OFF(x)               ((x) % BITS_PER_LONG)
-#define LONG(x)              ((x) / BITS_PER_LONG)
-#define test_bit(bit, array) ((array[LONG(bit)] >> OFF(bit)) & 1)
-
-void dev_input_map_query() {
+void dev_input_scan() {
   uv_fs_t fs_dir_req;
   if (!uv_fs_scandir(uv_default_loop(), &fs_dir_req, DEV_INPUT_PATH, 0, NULL)) return;
 
@@ -88,196 +92,126 @@ void dev_input_map_query() {
       continue;
     }
 
-    int i, j;
-    int version;
-    unsigned short id[4];
-    unsigned long bit[EV_MAX][NBITS(KEY_MAX)];
+    dev_input_t dev;
+    if (!dev_input_query(&dev, fd)) continue;
 
-    if (ioctl(fd, EVIOCGVERSION, &version)) {
-      continue;
+    printf("path:             " DEV_INPUT_PATH "event%d\n", device);
+    printf("name:             %s\n", dev.name);
+    printf("driver version:   %d.%d.%d\n", dev.driver_version >> 16, (dev.driver_version >> 8) & 0xff, dev.driver_version & 0xff);
+    printf("id bus type:      %d\n", dev.ids.bustype);
+    printf("id product:       %d\n", dev.ids.product);
+    printf("id vendor:        %d\n", dev.ids.vendor);
+    printf("id version:       %d\n", dev.ids.version);
+
+    printf("Supported events: ");
+    for (int i = 0; i < EV_CNT; i++) {
+      if (bit_is_set(dev.bits, i)) {
+        switch (i) {
+          case EV_SYN:
+            printf("EV_SYN ");
+            break;
+          case EV_KEY:
+            printf("EV_KEY ");
+            break;
+          case EV_REL:
+            printf("EV_REL ");
+            break;
+          case EV_ABS:
+            printf("EV_ABS ");
+            break;
+          case EV_MSC:
+            printf("EV_MSC ");
+            break;
+          case EV_SW:
+            printf("EV_SW ");
+            break;
+          case EV_LED:
+            printf("EV_LED ");
+            break;
+          case EV_SND:
+            printf("EV_SND ");
+            break;
+          case EV_REP:
+            printf("EV_REP ");
+            break;
+          case EV_FF:
+            printf("EV_FF ");
+            break;
+          case EV_PWR:
+            printf("EV_PWR ");
+            break;
+          case EV_FF_STATUS:
+            printf("EV_FF_STATUS ");
+            break;
+        }
+      }
     }
-    printf("path:           " DEV_INPUT_PATH "event%d\n", device);
-    printf("driver version: %d.%d.%d\n", version >> 16, (version >> 8) & 0xff, version & 0xff);
+    printf("\n");
 
-    ioctl(fd, EVIOCGID, id);
-    printf("device bus:     0x%x\n", id[ID_BUS]);
-    printf("device vendor:  0x%x\n", id[ID_VENDOR]);
-    printf("device product: 0x%x\n", id[ID_PRODUCT]);
-    printf("device version: 0x%x\n", id[ID_VERSION]);
-
-    // memset(bit, 0, sizeof(bit));
-    // ioctl(fd, EVIOCGBIT(0, EV_MAX), bit[0]);
-    // printf("Supported events:\n");
-    // for (i = 0; i < EV_MAX; i++) {
-    //   if (test_bit(i, bit[0])) {
-    //     printf("  Event type %d\n", i);
-    //     if (!i) continue;
-    //     ioctl(fd, EVIOCGBIT(i, KEY_MAX), bit[i]);
-    //     for (j = 0; j < KEY_MAX; j++) {
-    //       if (test_bit(j, bit[i])) {
-    //         printf("%d, ", j);
-    //       }
-    //     }
-    //     printf("\n");
-    //   }
-    // }
     printf("\n");
   }
 
   uv_fs_req_cleanup(&fs_dir_req);
 }
 
-// #define _GNU_SOURCE /* for asprintf */
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <dirent.h>
-// #include <errno.h>
-// #include <linux/input.h>
+int dev_input_query(dev_input_t* dev, int fd) {
+  int rc;
 
-// #define BITS_PER_LONG (sizeof(long) * 8)
-// #define NBITS(x) ((((x)-1)/BITS_PER_LONG)+1)
-// #define OFF(x)  ((x)%BITS_PER_LONG)
-// #define LONG(x) ((x)/BITS_PER_LONG)
-// #define test_bit(bit, array)	((array[LONG(bit)] >> OFF(bit)) & 1)
+  if (fd < 0) return 0;
 
-// #define DEV_INPUT_EVENT "/dev/input"
-// #define EVENT_DEV_NAME "event"
+  rc = ioctl(fd, EVIOCGBIT(0, sizeof(dev->bits)), dev->bits);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGBIT(0, sizeof(dev->bits)), dev->bits);\n");
+    return 0;
+  }
 
-// int is_event_device(const struct dirent *dir) {
-// 	return strncmp(EVENT_DEV_NAME, dir->d_name, 5) == 0;
-// }
+  rc = ioctl(fd, EVIOCGNAME(sizeof(dev->name) - 1), dev->name);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGNAME(sizeof(dev->name) - 1), dev->name);\n");
+    return 0;
+  }
 
-// char* scan_devices(void) {
-// 	struct dirent **namelist;
-// 	int i, ndev, devnum;
-// 	char *filename;
+  rc = ioctl(fd, EVIOCGPHYS(sizeof(dev->phys) - 1), dev->phys);
+  if (rc < 0 && errno != ENOENT) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGPHYS(sizeof(dev->phys) - 1), dev->phys);\n");
+    return 0;
+  }
 
-// 	ndev = scandir(DEV_INPUT_EVENT, &namelist, is_event_device, alphasort);
-// 	if (ndev <= 0) {
-// 		return NULL;
-// 	}
+  rc = ioctl(fd, EVIOCGUNIQ(sizeof(dev->uniq) - 1), dev->uniq);
+  if (rc < 0 && errno != ENOENT) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGUNIQ(sizeof(dev->uniq) - 1), dev->uniq);\n");
+    return 0;
+  }
 
-// 	printf("Available devices:\n");
+  rc = ioctl(fd, EVIOCGID, &dev->ids);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGID, &dev->ids);\n");
+    return 0;
+  }
 
-// 	for (i = 0; i < ndev; i++) {
-// 		char fname[64];
-// 		int fd = -1;
-// 		char name[256] = "???";
+  rc = ioctl(fd, EVIOCGVERSION, &dev->driver_version);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGVERSION, &dev->driver_version);\n");
+    return 0;
+  }
 
-// 		snprintf(fname, sizeof(fname), "%s/%s", DEV_INPUT_EVENT,
-// namelist[i]->d_name); 		fd = open(fname, O_RDONLY); 		if (fd >= 0) { 			ioctl(fd,
-// EVIOCGNAME(sizeof(name)), name); 			close(fd);
-// 		}
-// 		printf("%s:  %s\n", fname, name);
-// 		free(namelist[i]);
-// 	}
+  rc = ioctl(fd, EVIOCGBIT(EV_KEY, sizeof(dev->key_bits)), dev->key_bits);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGBIT(EV_KEY, sizeof(dev->key_bits)), dev->key_bits);\n");
+    return 0;
+  }
 
-// 	fprintf(stderr, "Select the device event number [0-%d]: ", ndev - 1);
-// 	scanf("%d", &devnum);
+  rc = ioctl(fd, EVIOCGKEY(sizeof(dev->key_values)), dev->key_values);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR: ioctl(fd, EVIOCGKEY(sizeof(dev->key_values)), dev->key_values);\n");
+    return 0;
+  }
 
-// 	if (devnum >= ndev || devnum < 0) {
-// 		return NULL;
-// 	}
+  dev->fd = fd;
 
-// 	asprintf(&filename, "%s/%s%d", DEV_INPUT_EVENT, EVENT_DEV_NAME, devnum);
-// 	return filename;
-// }
+  return 1;
+}
 
-// static int print_device_info(int fd) {
-// 	int i, j;
-// 	int version;
-// 	unsigned short id[4];
-// 	unsigned long bit[EV_MAX][NBITS(KEY_MAX)];
-
-// 	if (ioctl(fd, EVIOCGVERSION, &version)) {
-// 		perror("can't get version");
-// 		return 1;
-// 	}
-// 	printf("Input driver version is %d.%d.%d\n",
-// 	       version >> 16, (version >> 8) & 0xff, version & 0xff);
-
-// 	ioctl(fd, EVIOCGID, id);
-// 	printf("Input device ID: bus 0x%x vendor 0x%x product 0x%x version
-// 0x%x\n", 		id[ID_BUS], id[ID_VENDOR], id[ID_PRODUCT], id[ID_VERSION]);
-
-// 	memset(bit, 0, sizeof(bit));
-// 	ioctl(fd, EVIOCGBIT(0, EV_MAX), bit[0]);
-// 	printf("Supported events:\n");
-// 	for (i = 0; i < EV_MAX; i++) {
-//  		if (test_bit(i, bit[0])) {
-// 			printf("  Event type %d\n", i);
-// 			if (!i) continue;
-// 			ioctl(fd, EVIOCGBIT(i, KEY_MAX), bit[i]);
-// 			for (j = 0; j < KEY_MAX; j++) {
-// 				if (test_bit(j, bit[i])) {
-// 					printf("%d, ", j);
-// 				}
-// 			}
-// 			printf("\n");
-// 		}
-// 	}
-// 	return 0;
-// }
-
-// int print_events(int fd) {
-// 	struct input_event ev;
-// 	unsigned int size;
-
-// 	printf("Testing ... (interrupt to exit)\n");
-
-// 	while (1) {
-// 		size = read(fd, &ev, sizeof(struct input_event));
-
-// 		if (size < sizeof(struct input_event)) {
-// 			printf("expected %lu bytes, got %u\n", sizeof(struct
-// input_event), size); 			perror("\nerror reading"); 			return EXIT_FAILURE;
-// 		}
-
-// 		printf("Event: time %ld.%06ld, ", ev.time.tv_sec,
-// ev.time.tv_usec); 		printf("type: %i, code: %i, value: %i\n", ev.type, ev.code,
-// ev.value);
-// 	}
-// }
-
-// int main () {
-// 	int fd, grabbed;
-// 	char *filename;
-
-// 	if (getuid() != 0) {
-// 		fprintf(stderr, "Not running as root, no devices may be
-// available.\n");
-// 	}
-
-// 	filename = scan_devices();
-// 	if (!filename) {
-// 		fprintf(stderr, "Device not found\n");
-// 		return EXIT_FAILURE;
-// 	}
-
-// 	if ((fd = open(filename, O_RDONLY)) < 0) {
-// 		perror("");
-// 		if (errno == EACCES && getuid() != 0) {
-// 			fprintf(stderr, "You do not have access to %s. Try "
-// 					"running as root instead.\n", filename);
-// 		}
-// 		return EXIT_FAILURE;
-// 	}
-
-// 	free(filename);
-
-// 	if (print_device_info(fd)) {
-// 		return EXIT_FAILURE;
-// 	}
-
-// 	grabbed = ioctl(fd, EVIOCGRAB, (void *) 1);
-// 	ioctl(fd, EVIOCGRAB, (void *) 0);
-// 	if (grabbed) {
-// 		printf("This device is grabbed by another process. Try switching
-// VT.\n"); 		return EXIT_FAILURE;
-// 	}
-
-// 	return print_events(fd);
-// }
+static inline int bit_is_set(const unsigned long* array, int bit) {
+  return !!(array[bit / LONG_BITS] & (1LL << (bit % LONG_BITS)));
+}
